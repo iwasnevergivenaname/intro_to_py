ec19d45284fdaa7a0f084a89b74d3bf9a93a00a3
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-09-14 14:17:50 -0700

    instances of coffeecup

--- coffee.py
+++ coffee.py
@@ -12,7 +12,29 @@ class CoffeeCup():
         self.amount = 0
 
     def drink(self, amount):
-        self.mount -= amount
+        self.amount -= amount
         if (self.amount == 0):
             self.amount = 0
             print("this bitch empty yeet")
+
+# number argument is capacity
+han_cup = CoffeeCup(8)
+gerard_cup = CoffeeCup(18)
+bert_cup = CoffeeCup(2)
+pete_cup = CoffeeCup(3)
+
+bert_cup.empty()
+
+han_cup.fill()
+gerard_cup.fill()
+bert_cup.fill()
+pete_cup.fill()
+
+gerard_cup.drink(6)
+
+print(han_cup.amount)
+print(gerard_cup.amount)
+print(bert_cup.amount)
+print(pete_cup.amount)
+
+print(f"han only has {han_cup.amount} oz of coffee left")
